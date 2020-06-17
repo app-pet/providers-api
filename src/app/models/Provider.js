d339ba8919119901278cb5f049fd83679b1686ac
serginhoh <serginhoh@gmail.com>
2020-06-17 14:56:45 -0300

    ajuste url swagger

--- src/app/models/Provider.js
+++ src/app/models/Provider.js
@@ -18,6 +18,8 @@ const mongoosePaginate = require('mongoose-paginate')
  *              type: string
  *          services:
  *              type: array
+ *              items:
+ *                  type: string
  * 
  *        required:
  *          - name
